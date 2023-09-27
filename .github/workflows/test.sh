62ce1cd8bed3c6161c231280d5fe88c874080406
Oleg Voitiuk <olgv@protonmail.com>
2023-09-27 13:11:30 +0300

    Test shell script

--- .github/workflows/test.sh
+++ .github/workflows/test.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
-echo "TEST RUN SCRIPT"
+echo "Script Started"
+
+GIT_FILES_LIST=git ls-tree -r HEAD schemas | awk '{print $4}'
+
+SCHEMAS=$(echo "$GIT_FILES_LIST" | \
+          head -c -1 | \
+          jq -R -s -c 'split(" ")')
+
+echo "$GIT_FILES_LIST"
+echo "$SCHEMAS"
+
+echo "Script Ended"
